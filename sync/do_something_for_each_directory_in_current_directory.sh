cf5a4b2dc6da60c1f7897af83979df1bf84b7b4e
johnreubank <johnreubank@gmail.com>
2025-03-31 00:45:31 -0400

    address error in codespace log

--- sync/do_something_for_each_directory_in_current_directory.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-for dir in */; do
-  directory_name=${dir%/}
-  cat "$directory_name/README.md" >> "$directory_name/home.md"
-  rm "$directory_name/README.md"
-  echo "performed this action related to: $directory_name"
-done
